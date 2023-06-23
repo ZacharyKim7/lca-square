e12bbaf95fa90d5c3ae7cccb5c9d7f56a2258401
Zachary Kim <zacharykim7@gmail.com>
2023-06-22 22:05:58 -0700

    React SetUp

--- README.md
+++
@@ -1,2 +0,0 @@
-# LCA-Square
-Online hot lunch payment method using Square for Lake City Academy
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import styles from './style';
+
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${"sm:px-16 px-6"} ${"flex justify-center items-center"}`}>
+      <div className={`${"xl:max-w-[1280px] w-full"}`}>
+        Navbar
+      </div>
+    </div>
+
+    <div className = {`bg-primary ${"flex justify-center items-start"}`}>
+      <div className = {`${"xl:max-w-[1280px] w-full"}`}>
+        Hero
+      </div>
+    </div>
+
+    <div className = {`bg-primary ${"sm:px-16 px-6"} ${"flex justify-center items-start"}`}>
+      <div className = {`${"xl:max-w-[1280px] w-full"}`}>
+        Stats
+        Business
+        Billing
+        CardDeal
+        Testimonials
+        Clients
+        CTA
+        Footer
+      </div>
+    </div>
+  </div>
+);
+
+
+export default App
